dd7dd231aa7b838dfd6bcabd3df8d555a053ec09
HEITOR SCHWINDEN RACHADEL <heitor_rachadel@estudante.sesisenai.org.br>
2025-04-15 11:02:19 -0300

    jogo class

--- script.js
+++ script.js
@@ -40,6 +40,12 @@ class Player extends Entidade {
         super(x, y, altura, largura, cor);
         this.#velocidade_x = 5;
     }
+
+    desenhar() {
+        ctx.fillStyle = this.cor
+        ctx.fillRect(this.x, this.y, this.largura, this.altura)
+    }
+
     moverEsquerda() {
         this.x -= this.velocidadeX;
         if (this.x < 0) this.x = 0;
@@ -49,6 +55,24 @@ class Player extends Entidade {
         this.x += this.#velocidade_x
         if (this.x = this.largura > canvas.width) this.x = canvas.width - this.largura;
     }
+    atualizar() {
+        if (!Jogo.gameOver) {
+            if (teclaEsquerdaPressionada) this.moverEsquerda();
+            if (teclaDireitaPressionada) this.moverDireita();
+        }
+    }
+    verificarColisao(obstaculo) {
+        if (alien.x < this.x + this.largura &&
+            alien.x + alien.largura > this.x &&
+            this.y < alien.y + alien.altura &&
+            this.y + this.altura > alien.y) {
+            alien.pararAlien();
+            this.#velocidade_x = 0;
+            ctx.fillStyle = 'black';
+            ctx.font = '50px Arial';
+            ctx.fillText('GAME OVER', 300, 100);
+            Jogo.gameOver = true;
+        }}
 }
 
 class Aliens extends Entidade {
@@ -61,15 +85,32 @@ class Aliens extends Entidade {
     cair() {
         this.y -= this.#velocidade_y
     }
+    pararAlien() {
+        this.velocidade_x = 0;
+    }
+
+    desenhar() {
+        ctx.fillStyle = this.cor
+        ctx.fillRect(this.x, this.y, this.largura, this.altura)
+    }
 }
 
-const objeto_na_tela = new Entidade(50, 50, 50, 50, 'red')
+class Jogo{
+    constructor(){
+        this.player = new Player (375, 340, 30, 50, 'white');
+        this.aline = [
+            new Aliens (30, 10, 30 ,50, "green"),
 
+        ];
+        this.loop = this.loop.bind(this);
+        this.contadorFrames = 0; 
+    }
+}
 
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    objeto_na_tela.desenhar()
-    Aliens.cair()
+    player.desenhar()
+    alien.desenhar()
     requestAnimationFrame(loop)
 }
 loop()
